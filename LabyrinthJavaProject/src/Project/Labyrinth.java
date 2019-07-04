fc6e480ad1a1125a36665d09f775578c430f7d7b
Eg0R0828 <egorhh357@gmail.com>
2019-07-04 13:02:55 +0300

    refactoring

---
+++ LabyrinthJavaProject/src/Project/Labyrinth.java
@@ -0,0 +1,20 @@
+package Project;
+
+import Project.DrawLabyrinth.*;
+
+// Changing a work-style: random or file;
+// Calling a flood-fill algorithm;
+
+public class Labyrinth {
+    // here will be a pathes massive;
+
+    // file-read constructor;
+    public Labyrinth(String filePath){
+        DrawLabyrinth window = new DrawLabyrinth();
+    }
+
+    // random generate constructor;
+    public Labyrinth(int patternNumber){
+        DrawLabyrinth window = new DrawLabyrinth();
+    }
+}
