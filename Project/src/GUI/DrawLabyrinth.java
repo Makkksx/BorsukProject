0a4e0df5b92ee65e1697fda89c0aad1cdf50afaf
Eg0R0828 <egorhh357@gmail.com>
2019-07-06 22:06:12 +0300

    Random Generating

--- Project/src/GUI/DrawLabyrinth.java
+++ Project/src/GUI/DrawLabyrinth.java
@@ -1,30 +1,68 @@
 package GUI;
 
+import Matrix.Labyrinth;
+
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.geom.*;
 import javax.swing.*;
 
 // Drawing the window with labyrinth by matrix;
 
 public class DrawLabyrinth {
-    JRadioButton min_path = new JRadioButton("Min Path");
-    JRadioButton max_path = new JRadioButton("Max Path");
+    public static int SIZE;
+    public static final int WIDTH = 50;
+    public static final int HIGH = 50;
+    public Labyrinth labyrinth;
 
-    public DrawLabyrinth() {
+    public DrawLabyrinth(Labyrinth labyrinth) {
+        SIZE = labyrinth.getSize();
+        this.labyrinth = labyrinth;
         JFrame frame = new JFrame("Generated Labyrinth");
-        frame.setBounds(630, 150, 650, 650);
-        JPanel container = new JPanel();
-        frame.setLayout(null);
-        frame.getContentPane().add(container);
+        frame.setBounds(800, 200, WIDTH* (SIZE+1), HIGH* (SIZE+1));
+        frame.add(new myComponent());
         frame.setVisible(true);
-        // pathes;
-        min_path.setBounds(50, 10, 200, 20);
-        max_path.setBounds(400, 10, 200, 20);
-        ButtonGroup patternGroup = new ButtonGroup();
-        patternGroup.add(min_path);
-        patternGroup.add(max_path);
-        frame.add(min_path);
-        min_path.setSelected(true); // Selected in a default mode;
-        frame.add(max_path);
+    }
+
+    class myComponent extends JComponent {
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            g.setColor(Color.WHITE);
+            g.fillRect(0,0, WIDTH* SIZE, HIGH * SIZE);
+            paintLab(g2);
+            paintField(g2);
+        }
+    }
+
+    public void paintLab(Graphics2D g) {
+        for (int i=0;i<SIZE;i++) {
+            for (int j=0;j<SIZE;j++) {
+                switch (labyrinth.getCell(i,j)) {
+                    case '1':
+                        g.setColor(Color.GRAY);
+                        g.fillRect(i*WIDTH,j*HIGH,WIDTH,HIGH);
+                        break;
+                    case 's':
+                        g.setColor(Color.YELLOW);
+                        g.fillRect(i*WIDTH,j*HIGH,WIDTH,HIGH);
+                        break;
+                    case 'f':
+                        g.setColor(Color.RED);
+                        g.fillRect(i*WIDTH,j*HIGH,WIDTH,HIGH);
+                        break;
+                    default:
+                }
+            }
+        }
+    }
+
+    public void paintField(Graphics2D g) {
+        g.setColor(Color.BLACK);
+        for (int xx = WIDTH; xx <= WIDTH* SIZE; xx+=WIDTH) {
+            g.draw(new Line2D.Double(xx, 0, xx, HIGH * SIZE));
+        }
+        for (int yy = HIGH; yy <= HIGH * SIZE; yy+=HIGH) {
+            g.draw(new Line2D.Double(0, yy,WIDTH* SIZE, yy ));
+        }
     }
 }
--- Project/src/Matrix/Labyrinth.java
+++ Project/src/Matrix/Labyrinth.java
@@ -1,11 +1,21 @@
 package Matrix;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Labyrinth {
     private static char [][] labyrinth;
     private static int size;
+    private ArrayList<Integer> pathes_x = new ArrayList<Integer>();
+    private ArrayList<Integer> pathes_y = new ArrayList<Integer>();
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCell(int i, int j) {
+        return labyrinth[j][i];
+    }
 
     public Labyrinth(File file) throws IOException {
         String line;
@@ -22,23 +32,61 @@ public class Labyrinth {
         }
     }
 
-    public void floodFill(int x, int y) {
-        if(checker(x, y)) {
-            if(labyrinth[x][y] == 'f')
-                labyrinth[x][y] = 'r';
-            else
-                labyrinth[x][y] = '1';
+    public void save(File file) throws IOException {
+        FileWriter fw = new FileWriter(file);
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                fw.write(labyrinth[i][j]);
+            }
+            fw.write("\n");
         }
-        if(checker(x + 1, y))
-            floodFill(x + 1, y);
-        if(checker(x - 1, y))
-            floodFill(x - 1, y);
-        if(checker(x, y + 1))
-            floodFill(x, y + 1);
-        if(checker(x, y - 1))
-            floodFill(x, y - 1);
+        fw.close();
     }
-    public void  printLabyrinth(){
+
+    public boolean floodFill(int x, int y) {
+        if(labyrinth[x][y] == 'f') return true;
+        else {
+            if (labyrinth[x][y] != 's')
+                labyrinth[x][y] = '2';
+            if (checker(x + 1, y)) {
+                if (floodFill(x + 1, y)) {
+                    pathes_x.add(x + 1);
+                    pathes_y.add(y);
+                    return true;
+                }
+            }
+            if (checker(x - 1, y)) {
+                if (floodFill(x - 1, y)) {
+                    pathes_x.add(x - 1);
+                    pathes_y.add(y);
+                    return true;
+                }
+            }
+            if (checker(x, y + 1)) {
+                if (floodFill(x, y + 1)) {
+                    pathes_x.add(x);
+                    pathes_y.add(y + 1);
+                    return true;
+                }
+            }
+            if (checker(x, y - 1)) {
+                if (floodFill(x, y - 1)) {
+                    pathes_x.add(x);
+                    pathes_y.add(y - 1);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checker(int x, int y) {
+        if(x < 0 || y < 0 || x >= size || y >= size)
+            return false;
+        return labyrinth[x][y] == '0' || labyrinth[x][y] == 'f' ;
+    }
+
+    public void  printLabyrinth() {
         for(int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 System.out.print(labyrinth[i][j]);
@@ -48,11 +96,99 @@ public class Labyrinth {
         System.out.println(" ");
     }
 
-    private boolean checker(int x, int y){
-        if(x < 0 || y < 0 || x >= size || y >= size)
-            return false;
-        if(labyrinth[x][y] != '1')
-            return true;
-        else return false;
+    // Конструктор для рандомной генерации;
+    public Labyrinth(int n) throws IOException {
+        // Инициализация;
+        labyrinth = new char[n][n];
+        size = n;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || j == 0 || i == (size - 1) || j == (size - 1))
+                    labyrinth[i][j] = '0';
+                else labyrinth[i][j] = '1';
+            }
+        }
+
+        labyrinth[n/2][n/2] = '0';
+        digger(n/2, n/2);
+        boolean flag = true;
+        while (flag) {
+            int curr_x = 2, curr_y = 2;
+            boolean f = true;
+            for (int i = 2; i < (size - 2); i += 2) {
+                for (int j = 2; j < (size - 2); j += 2) {
+                    if (labyrinth[i][j] == '1') {
+                        curr_x = i;
+                        curr_y = j;
+                        f = false;
+                        break;
+                    }
+                    if (i == (size - 3) && j == (size - 3)) flag = false;
+                }
+                if (!f) break;
+            }
+            if (flag) {
+                labyrinth[curr_x][curr_y] = '0';
+                digger(curr_x, curr_y);
+            }
+        }
+
+        int count = size;
+        while (count > 0) {
+            int curr_x = (int)(Math.random() * (size - 2) + 1);
+            int curr_y = (int)(Math.random() * (size - 2) + 1);
+            while ((curr_x % 2 == 0 || curr_y % 2 == 0) || labyrinth[curr_x][curr_y] == '0' ||
+            (!(labyrinth[curr_x][curr_y + 1] == '0' && labyrinth[curr_x][curr_y - 1] == '0'
+            && labyrinth[curr_x + 1][curr_y] == '1' && labyrinth[curr_x - 1][curr_y] == '1') &&
+            !(labyrinth[curr_x + 1][curr_y] == '0' && labyrinth[curr_x - 1][curr_y] == '0'
+            && labyrinth[curr_x][curr_y + 1] == '1' && labyrinth[curr_x][curr_y - 1] == '1'))) {
+                curr_x = (int)(Math.random() * (size - 1) + 1);
+                curr_y = (int)(Math.random() * (size - 1) + 1);
+            }
+            labyrinth[curr_x][curr_y] = '0';
+            count--;
+        }
+
+        labyrinth[n/2][n/2] = 's';
+        labyrinth[size - 1][size - 1] = 'f';
+    }
+
+    private void digger(int curr_x, int curr_y) {
+        boolean up = true, down = true, left = true, right = true;
+        while (up || down || left || right) {
+            int direction = (int)(Math.random() * 4);
+            if (direction == 0) {
+                if (up && curr_x >= 0 && curr_y + 2 >= 0 && curr_x < size && curr_y + 2 < size && labyrinth[curr_x][curr_y + 2] != '0') {
+                    labyrinth[curr_x][curr_y + 1] = '0';
+                    labyrinth[curr_x][curr_y + 2] = '0';
+                    digger(curr_x, curr_y + 2);
+                    return;
+                } else up = false;
+            }
+            if (direction == 1) {
+                if (down && curr_x >= 0 && curr_y - 2 >= 0 && curr_x < size && curr_y - 2 < size && labyrinth[curr_x][curr_y - 2] != '0') {
+                    labyrinth[curr_x][curr_y - 1] = '0';
+                    labyrinth[curr_x][curr_y - 2] = '0';
+                    digger(curr_x, curr_y - 2);
+                    return;
+                } else down = false;
+            }
+            if (direction == 2) {
+                if (right && curr_x + 2 >= 0 && curr_y >= 0 && curr_x + 2 < size && curr_y < size && labyrinth[curr_x + 2][curr_y] != '0') {
+                    labyrinth[curr_x + 1][curr_y] = '0';
+                    labyrinth[curr_x + 2][curr_y] = '0';
+                    digger(curr_x + 2, curr_y);
+                    return;
+                } else right = false;
+            }
+            if (direction == 3) {
+                if (left && curr_x - 2 >= 0 && curr_y >= 0 && curr_x - 2 < size && curr_y < size && labyrinth[curr_x - 2][curr_y] != '0') {
+                    labyrinth[curr_x - 1][curr_y] = '0';
+                    labyrinth[curr_x - 2][curr_y] = '0';
+                    digger(curr_x - 2, curr_y);
+                    return;
+                } else left = false;
+            }
+        }
     }
 }
