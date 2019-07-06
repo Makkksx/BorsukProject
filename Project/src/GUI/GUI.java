df07b9690d78ab6ebf0028b8f32b9e35e5ce38b0
Eg0R0828 <egorhh357@gmail.com>
2019-07-06 22:12:36 +0300

    Random Generating

--- Project/src/GUI/GUI.java
+++ Project/src/GUI/GUI.java
@@ -5,140 +5,126 @@ import Matrix.Labyrinth;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.NoSuchElementException;
-import javax.imageio.*;
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
-
 
 public class GUI extends JFrame {
-    JLabel mainText = new JLabel("START MENU");
-    JRadioButton file_choice = new JRadioButton("InputFromFile");
-    JRadioButton random_choice = new JRadioButton("RandomInput");
-    JTextField input = new JTextField("", 1);
-    JLabel fileText = new JLabel("Enter a path to file if you had choose inputting from file!");
-    JLabel randomText = new JLabel("Enter a style of the labyrinth if you had choose a random generating!");
-    JRadioButton pattern1 = new JRadioButton("Pattern1");
-    JRadioButton pattern2 = new JRadioButton("Pattern2");
-    Button button = new Button("START");
     /**
      * I want this constructor to be a public!
      */
     public GUI() {
         /*Меню */
+        Container frameContainer = new Container();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImageIcon image_play =new ImageIcon("Pictures\\play.png");
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("Файл");
         JMenu new_file = new JMenu("Создать");
         JMenuItem open_file = new JMenuItem("Открыть");
+        JMenuItem play_button = new JMenuItem(image_play);
         JMenuItem save_file = new JMenuItem("Сохранить");
-        JMenuItem Pattern1 = new JMenuItem("Шаблон1");
-        JMenuItem Pattern2 = new JMenuItem("Шаблон2");
-        new_file.add(Pattern1);
-        new_file.add(Pattern2);
+        JMenuItem pattern1 = new JMenuItem("Шаблон1");
+        JMenuItem pattern2 = new JMenuItem("Шаблон2");
+        new_file.add(pattern1);
+        new_file.add(pattern2);
         fileMenu.add(new_file);
         fileMenu.add(open_file);
         fileMenu.add(save_file);
         menuBar.add(fileMenu);
+        menuBar.add(play_button);
+        frameContainer.add(menuBar);
         setJMenuBar(menuBar);
         setSize(800,600);
+        save_file.setEnabled(false);
+
         open_file.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                JFileChooser fileopen = new JFileChooser();
-                int ret = fileopen.showDialog(null, "Открыть файл");
+                JFileChooser fileOpen = new JFileChooser();
+                fileOpen.setCurrentDirectory(new File("."));
+                int ret = fileOpen.showDialog(null, "Открыть");
                 if (ret == JFileChooser.APPROVE_OPTION) {
                     try {
-                        Labyrinth labyrinth = new Labyrinth(fileopen.getSelectedFile());
+                        // Чтение лабиринта из файла;
+                        Labyrinth labyrinth = new Labyrinth(fileOpen.getSelectedFile());
+                        DrawLabyrinth drawLabyrinth = new DrawLabyrinth(labyrinth);
+                        // рисуем чистый лабиринт;
                         labyrinth.printLabyrinth();
-                        labyrinth.floodFill(1, 1);
+                        // Здесь должно быть указание координат старта и финиша. Пока строим один путь;
+                        labyrinth.floodFill(1, 1); // Вызов алгоритма;
+//                      //new DrawLabyrinth(labyrinth);
+                        // рисуем лабиринт с путем;
                         labyrinth.printLabyrinth();
-
-                    }catch(IOException e){
+                        // Если что-то открыли, можем результат записать в файл
+                        save_file.setEnabled(true);
+                        save_file.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                JFileChooser fileSave = new JFileChooser();
+                                fileSave.setCurrentDirectory(new File("."));
+                                fileSave.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                                int ret = fileSave.showDialog(null, "Сохранить");
+                                if (ret == JFileChooser.APPROVE_OPTION) {
+                                    File file = fileSave.getSelectedFile();
+                                    try {
+                                        labyrinth.save(file);
+                                        System.out.println("Save successfully");
+                                    } catch (IOException ex) {
+                                        ex.printStackTrace();
+                                    }
+                                }
+                            }
+                        });
+                    } catch(IOException e) {
                         System.out.println(e.getMessage());
                     }
-                    catch (NoSuchElementException e){
-
+                    catch (NoSuchElementException e) {
+                        System.exit(0);
                     }
                 }
             }
         });
-        /*
-        JMenu editMenu = new JMenu("Правка");
-        editMenu.add(new JMenuItem("Копировать"));
-        JMenu pasteMenu = new JMenu("Вставить");
-        pasteMenu.add(new JMenuItem("Из буфера"));
-        pasteMenu.add(new JMenuItem("Из файла"));
-        editMenu.add(pasteMenu);
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-        menuBar.add(Box.createHorizontalGlue());
-        menuBar.add(new JLabel(new ImageIcon("2.gif")));
-        setJMenuBar(menuBar);
-        setSize(1200,900);
-        /*
-        super("LabyrinthJavaProject - BorSukiTrushnikovTeam");
-        this.setBounds(100, 100, 1240, 720);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        JPanel container = new JPanel();
-        container.setLayout(null);
-        getContentPane().add(container);
-        //container.setLayout(new GridLayout(3, 2, 2, 2));
 
-        // main text;
-        container.add(mainText);
-        mainText.setBounds(150, 10, 100, 20);
-        // type of the labyrinth building;
-        file_choice.setBounds(20, 50, 200, 20);
-        random_choice.setBounds(250, 50, 200, 20);
-        ButtonGroup choiceGroup = new ButtonGroup();
-        choiceGroup.add(file_choice);
-        choiceGroup.add(random_choice);
-        container.add(file_choice);
-        file_choice.setSelected(true); // Selected in a default mode;
-        container.add(random_choice);
-        // file choice text;
-        container.add(fileText);
-        fileText.setBounds(20, 100, 400, 20);
-        // input a file path (file choice only);
-        input.setBounds(20, 130, 400, 20);
-        container.add(input);
-        // random choice text;
-        container.add(randomText);
-        randomText.setBounds(20, 160, 400, 20);
-        // style of the labyrinth random generating;
-        pattern1.setBounds(20, 190, 200, 20);
-        pattern2.setBounds(250, 190, 200, 20);
-        ButtonGroup patternGroup = new ButtonGroup();
-        patternGroup.add(pattern1);
-        patternGroup.add(pattern2);
-        container.add(pattern1);
-        pattern1.setSelected(true); // Selected in a default mode;
-        container.add(pattern2);
-        // a start button;
-        button.setBounds(140, 250, 100, 20);
-        button.addActionListener(new ButtonEventListener());
-        container.add(button);
-
-         */
-    }
-
-    private class ButtonEventListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            if (file_choice.isSelected()) {
-               // Labyrinth labyrinth = new Labyrinth(input.getText());
-            }
-            else {
-                // it will be more than 2 patterns;
-                if (pattern1.isSelected()) {
-                 //   Labyrinth labyrinth = new Labyrinth(1);
+        pattern1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    // Случайная генерация лабиринта;
+                    Labyrinth labyrinth = new Labyrinth(13);
+                    DrawLabyrinth drawLabyrinth = new DrawLabyrinth(labyrinth);
+                    // Рисуем чистый лабиринт;
+                    labyrinth.printLabyrinth();
+                    // Здесь должно быть указание координат старта и финиша. Пока строим один путь;
+                    labyrinth.floodFill(0, 0); // Вызов алгоритма;
+                    // Рисуем лабиринт с путем;
+                    labyrinth.printLabyrinth();
+                    // Если что-то открыли, можем результат записать в файл
+                    save_file.setEnabled(true);
+                    save_file.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            JFileChooser fileSave = new JFileChooser();
+                            fileSave.setCurrentDirectory(new File("."));
+                            fileSave.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                            int ret = fileSave.showDialog(null, "Сохранить");
+                            if (ret == JFileChooser.APPROVE_OPTION) {
+                                File file = fileSave.getSelectedFile();
+                                try {
+                                    labyrinth.save(file);
+                                    System.out.println("Save successfully");
+                                } catch (IOException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+                } catch(IOException e) {
+                    System.out.println(e.getMessage());
                 }
-                if (pattern2.isSelected()) {
-                   // Labyrinth labyrinth = new Labyrinth(2);
+                catch (NoSuchElementException e) {
+                    System.exit(0);
                 }
             }
-        }
+        });
     }
 }
